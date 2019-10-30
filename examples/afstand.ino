902da880dcaef96ee1e39c7b7c55ed3176eea098
Jørgen Christian Larsen <theresetmaster@gmail.com>
2019-10-30 15:19:07 +0100

    Create afstand.ino

---
+++ examples/afstand.ino
@@ -0,0 +1,13 @@
+#include <afstand.h>
+
+Afstandssensor afstandssensor(13, 12);  // Starter afstandssensoren på ben 13 og 12.
+
+void setup () {
+    Serial.begin(9600);  // Opsætter serial kommunikation tilbage til computeren
+}
+
+void loop () {
+    // Måler afstanden for hver 500ms
+    Serial.println(afstandssensor.afstand.CM());
+    delay(500);
+}
